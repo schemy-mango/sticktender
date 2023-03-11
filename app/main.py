6c70978e8b4b15891d6a3d58f1de955da77d840c
Marek Krysiak <125742882+schemy-mango@users.noreply.github.com>
2023-03-11 20:57:50 +0100

    Base files.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,34 @@
+#  Copyright (c) 2023. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+#  Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+#  Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+#  Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+#  Vestibulum commodo. Ut rhoncus gravida arcu.
+
+from typing import Union
+
+import uvicorn
 from PIL import Image
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    name_binominal: str
+    is_alive: Union[bool, None] = None
+    misting: Union[int, None] = None
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Ready."}
+
+
+@app.get("/plants/{plant_id}")
+def get_plant(plant_id: int):
+    return {"plant_id": plant_id}
 
 
 def main():
@@ -8,4 +38,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    uvicorn.run("app.main:app", port=5000, reload=True, access_log=False)
---
+++ tests/test_main.py
@@ -0,0 +1,20 @@
+#  Copyright (c) 2023. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+#  Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+#  Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+#  Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+#  Vestibulum commodo. Ut rhoncus gravida arcu.
+
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Ready."}
+
+# if __name__ == '__main__':
+#     test_read_root()
